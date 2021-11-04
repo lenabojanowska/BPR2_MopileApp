73bbd1d68b8fcda2338f604c6af774d20451c6af
lenabojanowska <43003653+lenabojanowska@users.noreply.github.com>
2021-11-04 23:08:05 +0100

    comment
    checking github and Android Studio connection on new branch

--- app/src/main/java/com/example/mobileapp/connection/ServiceGenerator.java
+++ app/src/main/java/com/example/mobileapp/connection/ServiceGenerator.java
@@ -16,7 +16,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceGenerator {
-
+    //comment
     private static OkHttpClient client = new OkHttpClient.Builder().build();
 
     private static Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
